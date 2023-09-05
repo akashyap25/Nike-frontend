41817421787eec7585518745481b35d5545cebf6
akashyap25 <anuragkashyap026@gmail.com>
2023-09-05 23:19:21 +0530

    Final Project

---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+
+export {Hero, CustomerReviews, Footer, SpecialOffers, Subscribe, SuperQuality, Services,PopularProducts}
